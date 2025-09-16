50a8b255e5d6648f0bf959f686e82370b7c08ce2
gulshanpatel5 <gulshanpatel044@gmail.com>
2025-09-16 15:11:29 +0530

    EveryThings is work don't touch ... stage- 1 completed.

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,10 +1,34 @@
-
-import React from 'react'
+"use client"
+import React, { useEffect, useRef } from 'react'
 import { Button } from './ui/button'
 import Link from 'next/link'
 import Image from 'next/image'
 
 const HeroSection = () => {
+  const imageRef = useRef(null);
+  useEffect(() =>{
+
+    const imageElement  = imageRef.current;
+    const handleScroll = ()=>{
+
+      
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+      if(scrollPosition> scrollThreshold){
+        imageElement.classList.add("scrolled");
+        
+      }else
+        imageElement.classList.remove("scrolled");
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return()=> window.removeEventListener("scroll", handleScroll);
+    
+  },[]);
+
+
+
+
   return (
     <section className='w-full pt-36 md:pt-48 pb-10'>
         <div className='space-y-6 text-center'>
@@ -14,12 +38,12 @@ const HeroSection = () => {
                     <br/>
                     Professional success
                 </h1>
-                <p>
+                <p className='mx-auto max-w-[600px] text-muted-foreground md:text-xl'>
                     Unlock your potential with personalized career guidance powered by AI.
                 </p>
             </div>
 
-            <div>
+            <div className=' flex justify-center space-x-4'>
               <Link href="/dashboard">
               <Button size="lg" className="px-8">
                Get Started
@@ -31,12 +55,12 @@ const HeroSection = () => {
               </Button>
               </Link> 
             </div>
-            <div>
-              <div>
+            <div className='hero-image-wrapper mt-5 md:mt-0'>
+              <div ref={imageRef} className='hero-image' >
                 <Image
                 src={"/banner.png"}
-                width={1334}
-                height={768}
+                width={1280}
+                height={720}
                 alt='Banner Anx'
                 className='rounded-lg shadow-2xl border mx-auto'
                 priority
